46319a4b45035377cd6069862a684055ed2eb817
William Barbosa <heytherewill@gmail.com>
2020-08-10 15:19:17 -0300

    :construction: Add structure for announcements

---
+++ Toggl.Core/Announcement.cs
@@ -0,0 +1,20 @@
+﻿namespace Toggl.Core
+{
+    public struct Announcement
+    {
+        public string Id { get; }
+        public string Title { get; }
+        public string Message { get; }
+        public string Url { get; }
+        public string CallToAction { get; set; }
+
+        public Announcement(string id, string title, string message, string url, string callToAction)
+        {
+            Id = id;
+            Url = url;
+            Title = title;
+            Message = message;
+            CallToAction = callToAction;
+        }
+    }
+}
