9430496257d04bd5ef7821ec47ca567775a85214
William Barbosa <heytherewill@gmail.com>
2020-08-11 06:30:06 -0300

    :bug: Fix order of strings

--- Toggl.Core/Announcement.cs
+++ Toggl.Core/Announcement.cs
@@ -6,9 +6,9 @@
         public string Title { get; }
         public string Message { get; }
         public string Url { get; }
-        public string CallToAction { get; set; }
+        public string CallToAction { get; }
 
-        public Announcement(string id, string title, string message, string url, string callToAction)
+        public Announcement(string id, string title, string message, string callToAction, string url)
         {
             Id = id;
             Url = url;
